a653f85cef82eb2502ff77ed6fc7ab7e3cea19b0
Vivaan Gupta <vivaangupta1304@gmail.com>
2022-10-31 19:16:06 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'adjasdasdnasldsainfdkflfknid' 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    
+    UPLOAD_LOCATION = '/Users/apple/Desktop/proj/drive/tmp'
